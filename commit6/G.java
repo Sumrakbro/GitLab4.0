61e37fc9770bbd0931e4205a27353c1941d15db6
Sumrakbro <artem_balakin_02@mail.ru>
2021-03-24 17:09:40 +0300

    Com6

---
+++ commit6/G.java
@@ -0,0 +1,30 @@
+public class G extends C {
+
+    private double e = 100.500;
+
+    private String b = "init";
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public void aa() {
+        System.out.println("Hello world!");
+    }
+
+    public int cc() {
+        return 42;
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public long dd() {
+        return 100500;
+    }
+}
