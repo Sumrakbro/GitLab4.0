91ee31f394d53e892404a0781201baac9e6b48e9
Artem <artem_balakin_02@mail.ru>
2021-03-24 17:02:30 +0300

    Com3

---
+++ commit3/C.java
@@ -0,0 +1,22 @@
+public class C implements H {
+
+    private int b = 42;
+
+    private long d = 4321;
+
+    public byte oo() {
+        return 4;
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public int af() {
+        return -1;
+    }
+}
