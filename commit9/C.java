a320962ab890285f8227d9b2c8e08e58d6d64927
Sumrakbro <artem_balakin_02@mail.ru>
2021-03-24 17:19:32 +0300

    Com9

---
+++ commit9/C.java
@@ -0,0 +1,42 @@
+public class C implements H {
+
+    private int b = 42;
+
+    private long d = 4321;
+
+    public byte oo() {
+        return 4;
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public Object gg() {
+        return return getClass().getClassLoader();
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public void ab() {
+        return;
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+}
---
+++ commit9/G.java
@@ -0,0 +1,34 @@
+public class G extends C {
+
+    private double e = 100.500;
+
+    private String b = "init";
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public void aa() {
+        System.out.println("Hello world!");
+    }
+
+    public int cc() {
+        return 42;
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public long dd() {
+        return 100500;
+    }
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+}
